77fa0d63d209317b9fb21b994cceaad364a641ae
Ivan Zakharkin <ivan.zakharkin296@gmail.com>
2021-04-08 21:54:49 +0300

    add render with mock data

--- src/main.js
+++ src/main.js
@@ -4,6 +4,15 @@ import { getPointEditTemplate } from './view/point-edit';
 import { getPointTemplate } from './view/point';
 import { getSortingTemplate } from './view/sorting';
 import { getTripInfoTemplate } from './view/trip-info';
+import generatePoints from './mock/points';
+
+const pointsContainer = document.querySelector('.trip-events__list');
+const points = generatePoints(20);
+//mock data already is sorted, but data from server may not be
+const sortedPointsByDate = points.sort(
+  (a, b) => a.dateFrom.unix() - b.dateFrom.unix(),
+);
+const editPoint = sortedPointsByDate.shift();
 
 const render = (container, tempalte, position) => {
   container.insertAdjacentHTML(position, tempalte);
@@ -23,33 +32,28 @@ const renderMenu = () => {
     'beforeend',
   );
 };
-
-const pointsContainer = document.querySelector('.trip-events__list');
-
-const renderPoints = () => {
-  const countPoint = 3;
-
-  for (let i = 0; i < countPoint; i += 1) {
-    render(pointsContainer, getPointTemplate(), 'beforeend');
-  }
+const renderPoints = (sortedPointsByDate) => {
+  sortedPointsByDate.forEach((point) =>
+    render(pointsContainer, getPointTemplate(point), 'beforeend'),
+  );
 };
-const renderPointEdit = () => {
-  render(pointsContainer, getPointEditTemplate(), 'afterbegin');
+const renderPointEdit = (editPoint) => {
+  render(pointsContainer, getPointEditTemplate(editPoint), 'afterbegin');
 };
 const renderSorting = () => {
   render(pointsContainer, getSortingTemplate(), 'beforebegin');
 };
-const renderTripInfo = () => {
+const renderTripInfo = (sortedPointsByDate) => {
   render(
     document.querySelector('.trip-main'),
-    getTripInfoTemplate(),
+    getTripInfoTemplate(sortedPointsByDate),
     'afterbegin',
   );
 };
 
 renderFilter();
 renderMenu();
-renderPoints();
-renderPointEdit();
+renderPoints(sortedPointsByDate);
+renderPointEdit(editPoint);
 renderSorting();
-renderTripInfo();
+renderTripInfo(sortedPointsByDate);
---
+++ src/mock/cities.js
@@ -0,0 +1,37 @@
+import { getRandomInteger } from '../utils';
+
+const names = ['Amsterdam', 'Chamonix', 'Geneva', 'Moscow', 'Los-Angeles'];
+const description =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.';
+
+const generateDescription = () => {
+  const sentences = description.split('.');
+  const countSentences = getRandomInteger(0, sentences.length - 1);
+
+  return sentences.splice(0, countSentences).join('.');
+};
+
+const generatePhotos = () => {
+  const count = getRandomInteger(0, 5);
+
+  return Array(count)
+    .fill()
+    .map(() => {
+      return {
+        src: `http://picsum.photos/248/152?r=${Math.random()}`,
+        description: generateDescription(),
+      };
+    });
+};
+
+const generateCities = () => {
+  return names.map((name) => {
+    return {
+      name,
+      description: generateDescription(),
+      photos: generatePhotos(),
+    };
+  });
+};
+
+export default generateCities();
